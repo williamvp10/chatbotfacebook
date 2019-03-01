39da8e4a1e4afc889f08e32b5bbb006e7ca3e30a
willi <willi@192.168.56.1>
2019-03-01 00:57:12 -0500

    prueba1

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -8,9 +7,7 @@ const app = express()
 
 // weatherman fb page
 const token = "EAAghpqdArs0BABZBBuirkF5PS171dxcVVLj5sn8V0id2hVikA9HoP11YacqKjb83mtXCx4o11bGjLsopIlvJxZAWtjKvzCGCTZBdSFyAD1eT485xjJZCSVEFNyjUZA29Rvi5KELOzl4CggZCs9SvExfWL4UOzdZAiaZCaoFPUy27HQZDZD";
-
 const msngerServerUrl = 'https://chatbotwilliam.herokuapp.com/bot';
-
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -39,186 +36,134 @@ app.listen(app.get('port'), function () {
     console.log('running on port', app.get('port'))
 })
 
-
-//FBM webhook
 app.post('/webhook/', function (req, res) {
     console.log(JSON.stringify(req.body));
-    let messaging_events = req.body.entry[0].messaging
+    let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
 
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-
-        let recipient = event.recipient.id
-        let time = req.body.entry[0].time
-
-        // we call the MessengerBot here..
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            //send it to the bot
+        let event = req.body.entry[0].messaging[i];
+        let sender = event.sender.id;
+        idusuario = sender;
+        let recipient = event.recipient.id;
+        let time = req.body.entry[0].time;
+        let text = "";
+        try {
+            text = req.body.entry[0].messaging[i].postback.title;
+            let type = req.body.entry[0].messaging[i].postback.payload;
+            console.log(type);
             request({
                 url: msngerServerUrl,
                 method: 'POST',
                 form: {
+                    'userType': type,
                     'userUtterance': text
                 }
-            },
-                    function (error, response, body) {
-                        //response is from the bot
-                        if (!error && response.statusCode == 200) {
-                            // Print out the response body
-                            //console.log(body)
-                            body = body.substring(1, body.length - 1);
-                            body = body.replace(/\\/g, '')
-                            console.log(body)
-                            let botOut = JSON.parse(body)
-
-                            if (botOut.botUtterance != null) {
-                                sendTextMessage(sender, botOut.botUtterance)
-                            }
-                        } else {
-                            sendTextMessage(sender, 'Error!')
-                        }
-                    });
+            }, function (error, response, body) {
+                //response is from the bot
+                if (!error && response.statusCode === 200) {
+                    selectTypeBotMessage(sender, body);
+                } else {
+                    sendTextMessage(sender, 'Error!');
+                }
+            });
+        } catch (err) {
+            sendtextbot(event, sender);
         }
+
     }
 
+    res.sendStatus(200);
+});
+function sendtextbot(event, sender) {
+    if (event.message && event.message.text) {
+        let text = event.message.text;
+        //send it to the bot
+        request({
+            url: msngerServerUrl,
+            method: 'POST',
+            form: {
+                'userUtterance': text
+            }
+        },
+                function (error, response, body) {
+                    //response is from the bot
+                    if (!error && response.statusCode === 200) {
+                        selectTypeBotMessage(sender, body);
+                    } else {
+                        sendTextMessage(sender, 'Error!');
+                    }
+                });
+    }
+}
+function selectTypeBotMessage(sender, body) {
+// Print out the response body
+    console.log(body);
+    body = body.substring(1, body.length - 1);
+    body = body.replace(/\\/g, '');
+    let botOut = JSON.parse(body);
+    if (botOut.botUtterance !== null) {
+        if (botOut.type !== null) {
+            var ty = botOut.type;
+            var t1 = "hola";
+            var n1 = ty.localeCompare(t1);
+            var t2 = "adios";
+            var n2 = ty.localeCompare(t2);
+            if (n1 === 0) {
+                sendTextMessageType(sender, botOut);
+            } else if (n2 === 0) {
+                sendTextMessageType(sender, botOut);
+            } else {
+                sendTextMessage(sender, "disculpa no puedo responder a tu solicitud");
+            }
 
-    res.sendStatus(200)
-})
 
-function sendTextMessage(sender, text) {
-    if (text != 'null') {
-        let messageData = {"attachment": {
-                "type": "image",
+        }
+        console.log(botOut.botUtterance);
+    }
+}
+function sendTextMessageType(sender, bot) {
+    let buttons = '[ ';
+    for (var i = 0; i < bot.buttons.length; i++) {
+        if (i !== 0) {
+            buttons += ',';
+        }
+        buttons += '{';
+        buttons += '"type": "postback",';
+        buttons += '"title": "' + bot.buttons[i].titulo + '",';
+        buttons += ' "payload": "' + bot.buttons[i].respuesta + '"';
+        buttons += '}';
+    }
+    buttons += ']';
+    console.log(buttons);
+    let b = JSON.parse(buttons);
+    if (bot !== 'null') {
+        let messageData = {
+            "attachment": {
+                "type": "template",
                 "payload": {
-                    "url": "https://www.google.com.co/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                    "template_type": "button",
+                    "text": bot.botUtterance,
+                    "buttons": b
                 }
             }
-        }
-
+        };
+        console.log(messageData);
+        // Start the request
         request({
             url: 'https://graph.facebook.com/v2.6/me/messages',
             qs: {access_token: token},
             method: 'POST',
             json: {
                 recipient: {id: sender},
-                message: messageData,
+                message: messageData
 
             }
         }, function (error, response, body) {
             if (error) {
-                console.log('Error sending messages: ', error)
+                console.log('Error sending messages: ', error);
             } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
+                console.log('Error: ', response.body.error);
             }
-        })
+        });
     }
 }
-
-
-//                              envio de una imagen 
-// let messageData = {"attachment": {
-//                "type": "image",
-//                "payload": {
-//                    "url": "https://www.google.com.co/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-//                }
-//            }
-//        }
-
-//                                   botones
-//let messageData = {
-//            "attachment": {
-//                "type": "template",
-//                "payload": {
-//                    "template_type": "button",
-//                    "text": "What do you want to do next?",
-//                    "buttons": [
-//                        {
-//                            "type": "web_url",
-//                            "url": "https://www.messenger.com",
-//                            "title": "Visit Messenger"
-//                        },
-//                        {
-//                            "type": "postback",   //error en el tipo postback
-//                            "title": "Weather this weekend",
-//                            "payload": "PAYLOAD_WEEKEND_LONDON"
-//                        },
-//                        {
-//                            "type": "Goto Website",
-//                            "url": "https://myweather.com/london",
-//                            "title": "More Info"
-//                        }
-//                    ]
-//                }
-//            }
-//        }
-
-
-
-
-//let messageData = {
-//            "attachment": {
-//                "type": "template",
-//                "payload": {
-//                    "template_type": "list",
-//                    "top_element_style": "compact",
-//                    "elements": [
-//                        {
-//                            "title": "Classic T-Shirt Collection",
-//                            "subtitle": "See all our colors",
-//                            "image_url": "https://www.google.com.co/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-//                            "buttons": [
-//                                {
-//                                    "title": "View",
-//                                    "type": "web_url",
-//                                    "url": "https://www.google.com",
-//                                    "messenger_extensions": true,
-//                                    "webview_height_ratio": "tall",
-//                                    "fallback_url": "https://www.google.com"
-//                                }
-//                            ]
-//                        },
-//                        {
-//                            "title": "Classic White T-Shirt",
-//                            "subtitle": "See all our colors",
-//                            "default_action": {
-//                                "type": "web_url",
-//                                "url": "https://www.google.com",
-//                                "messenger_extensions": false,
-//                                "webview_height_ratio": "tall"
-//                            }
-//                        },
-//                        {
-//                            "title": "Classic Blue T-Shirt",
-//                            "image_url": "https://www.google.com.co/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-//                            "subtitle": "100% Cotton, 200% Comfortable",
-//                            "default_action": {
-//                                "type": "web_url",
-//                                "url": "https://www.google.com",
-//                                "messenger_extensions": true,
-//                                "webview_height_ratio": "tall",
-//                                "fallback_url": "https://www.google.com"
-//                            },
-//                            "buttons": [
-//                                {
-//                                    "title": "Shop Now",
-//                                    "type": "web_url",
-//                                    "url": "https://www.google.com",
-//                                    "messenger_extensions": true,
-//                                    "webview_height_ratio": "tall",
-//                                    "fallback_url": "https://www.google.com"
-//                                }
-//                            ]
-//                        }
-//                    ],
-//                    "buttons": [
-//                        {
-//                            "title": "View More",
-//                            "type": "postback",
-//                            "payload": "payload"
-//                        }
-//                    ]
-//                }
-//            }
-//        }
